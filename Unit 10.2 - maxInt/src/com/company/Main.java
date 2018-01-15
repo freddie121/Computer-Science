0144e978daa25c79f2e926237be521741730edf4
Freddie <freddie.palmer@btinternet.com>
2018-01-15 10:22:51 +0000

    Add files via upload

---
+++ Unit 10.2 - maxInt/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+    public static int findMax(int[]listInt){
+        int maxInt = 0;
+        for(int i=0; i< listInt.length; i++ ){
+            if(listInt[i] > maxInt){
+                maxInt = listInt[i];
+            }
+        }
+        return maxInt;
+    }
+    public static void main(String[] args) {
+        int[] test = new int[0];
+        System.out.print(findMax(test));
+    }
+}
